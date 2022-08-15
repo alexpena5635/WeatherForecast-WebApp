034249c80e536036c9cf5bbacfdf3f1507f15e21
alexpena5635 <alexpena5635@gmail.com>
2022-08-14 22:19:51 -0600

    Furthered work on fleshing out server api for frontend to eventually query

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,20 @@
-const http  = require("http"),
-    url     = require("url"),
-    path    = require("path"),
-    fs      = require("fs")
-; 
+const http      = require("http"),
+    url         = require("url"),
+    path        = require("path"),
+    fs          = require("fs"),
+    WeatherAPI  = require("./weatherapi.js")
+;
 
 const hostname = "localhost";
 const port = 8080;
 
 // Directory to serve frontend files from
-const basedir = __dirname + "/../frontend/" 
-
-// With reference to: https://stackoverflow.com/a/26354478 
+const basedir = __dirname + "/../frontend/";
 
 const server = http.createServer((req, res) => {
     // Split code off into api and web server
-
     // API
-    if(req.url.startsWith("/api/")) {
+    if(req.url.startsWith("/api")) {
         api(req, res);
     }
     // Serve Files
@@ -29,12 +27,14 @@ server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+// Basic webserver to serve html, css, and js files to host the website
+// Webserver built with reference to: https://stackoverflow.com/a/26354478 
 function webserver(req, res) {
     // Identify the filename from the url (escaping with 'normalize')
     let uri = url.parse(req.url).pathname;
     let filepath = path.join(basedir, path.normalize(uri));
 
-    // Dict of file extensions corresponding to MIME types
+    // Dict of file extensions corresponding to mime types
     let mimeExtension = {
         '.html': "text/html",
         '.css' : "text/css",
@@ -85,14 +85,64 @@ function webserver(req, res) {
     });
 }
 
-function api(req, res) {
-    // set the request route
-    if (req.url === "/api/" && req.method === "GET") {
+// API functionality for the frontend to query
+async function api(req, res) {
+    // "/api/" contains 5 chars, the offset is 5
+    let apiOffset = 5; 
+    let endpoint = req.url.substring(apiOffset);
+
+    let locationRegex = "^(location|(location\/[a-zA-Z(%20)]+))(\/)?$";
+    let forecastRegex = "^(forecast|(forecast\/[0-9]+))(\/)?$";
+
+    // Default request route
+    if (endpoint === "" && req.method === "GET") {
         res.writeHead(200, { "Content-Type": "application/json" });
         res.write("API Test!");
         res.end();
     }
-    // no route present
+    // "/api/location/<city>"
+    else if (endpoint.match(locationRegex) && req.method === "GET") {
+        // Split the URL into pieces, and grab the last path
+        // Should correspond to the "<city>" part of URL
+        let pieces = endpoint.split('/');
+        let city = pieces.pop() || pieces.pop();
+
+        console.log("city: " + city);
+
+
+        let data = "{\"locations\":[]}";
+        try {
+            data = await new WeatherAPI().getLocation(city);
+        }
+        catch (e) {
+            console.error(e);
+        }
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(data);
+        res.end();
+    }
+    // "/api/forecast/daily/<locationID>"
+    else if (endpoint.match(forecastRegex) && req.method === "GET") {
+        // Split the URL into pieces, and grab the last path
+        // Should correspond to the "<locationID>" part of URL
+        let pieces = endpoint.split('/');
+        let locationID = pieces.pop() || pieces.pop();
+    
+        // Query the weather API to get the 7 day forecast data
+        let data = "{\"forecast\":[]}";
+        try {
+            data = await new WeatherAPI().getForecast(locationID);
+        }
+        catch (e) {
+            console.error(e);
+        }
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(data);
+        res.end();
+    }
+    // Non-existent route
     else {
         res.writeHead(404, { "Content-Type": "application/json" });
         res.end(JSON.stringify({ message: "Route not found" }));
