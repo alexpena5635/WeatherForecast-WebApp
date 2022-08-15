070238ddbad3e54922f619e58ca113e1ea28240c
alexpena5635 <alexpena5635@gmail.com>
2022-08-14 22:19:08 -0600

    Added weatherapi class to handle connection to Foreca weather api.

---
+++ backend/weatherapi.js
@@ -0,0 +1,34 @@
+const fs    = require("fs");
+var axios   = require("axios").default;
+
+const tokenFile = "./auth.txt";
+
+// Set the default url for axios to the weather api
+let token = fs.readFileSync(tokenFile, "utf-8");
+axios.defaults.baseURL = "https://fnw-us.foreca.com/api/v1";
+axios.defaults.headers.common = {'Authorization': `Bearer ${token}`}
+
+// Class to handle transactions with Foreca weather api using axios
+class WeatherAPI {
+    // Get location data from the Foreca api
+    async getLocation(location) {
+        const response = await axios.get("/location/search/" + location, {
+            responseType: "json",
+            transformResponse: [v => v]
+        });
+
+        return response.data;
+    }
+
+    // Get forecast data from the Foreca api, selecting the full datqaset
+    async getForecast(locationID) {
+        const response = await axios.get("/forecast/daily/" + locationID + "?dataset=full", {
+            responseType: "json",
+            transformResponse: [v => v]
+        });
+
+        return response.data;
+    }
+}
+
+module.exports = WeatherAPI;
